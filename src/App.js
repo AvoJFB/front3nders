491b98169d1c61d07704c0f9fd6d62712f109ea6
avetik <avetik.harutyunyan@logicify.com>
2017-11-25 11:52:20 +0200

    Integrate redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
 import { MuiThemeProvider, createMuiTheme } from 'material-ui';
+import { Provider } from 'react-redux';
+import configureStore from './store';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import DashboardComponent from "./components/DashboardComponent/index";
+import DashboardComponent from './components/DashboardComponent';
 
 const theme = createMuiTheme({
   direction: 'ltr',
 });
 
+const store = configureStore();
+
 class App extends Component {
   render() {
     return (
-        <BrowserRouter>
-          <MuiThemeProvider theme={theme}>
-          <div>
-            <Switch>
-              <Route path='/' component={DashboardComponent} />
-            </Switch>
-          </div>
-          </MuiThemeProvider>
-        </BrowserRouter>
-
+        <Provider store={store}>
+          <BrowserRouter>
+            <MuiThemeProvider theme={theme}>
+              <div>
+                <Switch>
+                  <Route path='/' component={DashboardComponent} />
+                </Switch>
+              </div>
+            </MuiThemeProvider>
+          </BrowserRouter>
+        </Provider>
     );
   }
 }
